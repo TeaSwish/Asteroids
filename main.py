39eee5b533ba6564dd66e40ec7dd947f4c7660e5
TeaSwish <srchoate1@gmail.com>
2025-07-31 07:39:23 -0700

    B: drew a dang ol' triangle in the middle of the screen and capped fps to 60

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    pygame.init
-    print("Starting Asteroids!")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #find the middle of the screen, to place player
+    player_initial_x = SCREEN_WIDTH / 2
+    player_initial_y = SCREEN_HEIGHT / 2
+    player = Player(player_initial_x, player_initial_y, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(0)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
